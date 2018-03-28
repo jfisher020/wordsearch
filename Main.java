fde8f45a6ded02ff642efee4069c18becf97dced
icowen <34110641+icowen@users.noreply.github.com>
2018-03-28 15:29:00 -0500

    Main class now generates and solves a wordsearch

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ package wordsearch;
  * Small word search solver.
  * @author Flip van Rijn
  */
-public class Main
+public class Main extends Wordsearch
 {
 
     /**
@@ -12,20 +12,24 @@ public class Main
      */
     public static void main(String[] args)
     {
-        char[][] grid = {
-            {'v', 'n', 'y', 'b', 'k', 'g', 's'},
-            {'r', 'o', 'r', 'a', 'n', 'g', 'e'},
-            {'e', 't', 'r', 'n', 'x', 'w', 'p'},
-            {'l', 'a', 'e', 'a', 'l', 'k', 'a'},
-            {'p', 'm', 'h', 'n', 'w', 'm', 'r'},
-            {'p', 'o', 'c', 'a', 'x', 'b', 'g'},
-            {'a', 't', 'n', 'o', 'm', 'e', 'l'}
-        };
+/*        char[][] grid = {
+*            {'v', 'n', 'y', 'b', 'k', 'g', 's'},
+*            {'r', 'o', 'r', 'a', 'n', 'g', 'e'},
+*            {'e', 't', 'r', 'n', 'x', 'w', 'p'},
+*            {'l', 'a', 'e', 'a', 'l', 'k', 'a'},
+*            {'p', 'm', 'h', 'n', 'w', 'm', 'r'},
+*            {'p', 'o', 'c', 'a', 'x', 'b', 'g'},
+*            {'a', 't', 'n', 'o', 'm', 'e', 'l'}
+*        };
+*          String[] words = {
+*            "apple", "banana", "cherry", "grapes",
+*            "lemon", "orange", "tomato", "ham"
+*          };
+*/
+        Wordsearch test = new Wordsearch();
+        char[][] grid = test.getBoard();
+        String[] words = test.getWords();
 
-        String[] words = {
-            "apple", "banana", "cherry", "grapes",
-            "lemon", "orange", "tomato", "ham"
-        };
 
         Solver solver = new Solver(grid, words);
         solver.solve();
